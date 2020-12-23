492d04744e9d48bbdedd80cad13e6a690cc07e5c
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-12-24 00:56:03 +0300

    feat: add guild commands

--- kotraction-test/src/main/kotlin/me/ihdeveloper/kotraction/test/commands.kt
+++ kotraction-test/src/main/kotlin/me/ihdeveloper/kotraction/test/commands.kt
@@ -3,7 +3,11 @@ package me.ihdeveloper.kotraction.test
 import me.ihdeveloper.kotraction.SlashCommands
 import me.ihdeveloper.kotraction.utils.slashCommands
 
+private const val TEST_GUILD_ID = "398407326798970880";
+
 internal fun commands(): SlashCommands {
     return slashCommands {
+        guildCommand("hello", TEST_GUILD_ID) {
+        }
     }
 }
--- src/main/kotlin/me/ihdeveloper/kotraction/core.kt
+++ src/main/kotlin/me/ihdeveloper/kotraction/core.kt
@@ -1,3 +1,24 @@
 package me.ihdeveloper.kotraction
 
-class SlashCommands
+private typealias GuildID = String
+
+class SlashCommands {
+    private val guildCommands = mutableMapOf<GuildID, MutableList<GuildCommand>>()
+
+    fun guildCommand(name: String, id: String, block: GuildCommand.() -> Unit) {
+        guildCommands.getOrDefault(id, arrayListOf()).run {
+            val command = GuildCommand(name, id)
+            block(command)
+            add(command)
+        }
+    }
+}
+
+abstract class Command(
+    val name: String
+)
+
+class GuildCommand(
+    name: String,
+    private val guildId: String,
+) : Command(name)
