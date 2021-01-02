bd98bce5f28aa2123e8d03f0ee885614539d7488
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2021-01-02 07:46:07 +0300

    chore(test): integrate register commands
    - read secrets file to extract secrets data
    - add description for the command

--- kotraction-test/src/main/kotlin/me/ihdeveloper/kotraction/test/commands.kt
+++ kotraction-test/src/main/kotlin/me/ihdeveloper/kotraction/test/commands.kt
@@ -3,11 +3,12 @@ package me.ihdeveloper.kotraction.test
 import me.ihdeveloper.kotraction.SlashCommands
 import me.ihdeveloper.kotraction.utils.slashCommands
 
-private const val TEST_GUILD_ID = "398407326798970880";
+private const val TEST_GUILD_ID = "211543198651121664"
 
 internal fun commands(): SlashCommands {
     return slashCommands {
         guildCommand("hello", TEST_GUILD_ID) {
+            description = "Prints hello world message"
         }
     }
 }
--- kotraction-test/src/main/kotlin/me/ihdeveloper/kotraction/test/main.kt
+++ kotraction-test/src/main/kotlin/me/ihdeveloper/kotraction/test/main.kt
@@ -2,6 +2,43 @@
 
 package me.ihdeveloper.kotraction.test
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import me.ihdeveloper.kotraction.Kotraction
+import java.io.File
+
+@Serializable
+internal data class SecretData(
+    val id: String,
+    val bot: Boolean,
+    val token: String,
+)
+
 fun main() {
     println("Starting Kotration Test...")
+
+    val secretFile = File("secret.json")
+
+    if (!secretFile.exists())
+        error("File [/'secret.json'] doesn't exist")
+
+    val secretData = readSecret(secretFile)
+
+    val kotraction = Kotraction(
+            applicationId = secretData.id,
+            token = secretData.token,
+            bot = secretData.bot,
+            slashCommands = commands(),
+    )
+
+    println("Registering slash guild commands...")
+    kotraction.registerCommands()
+}
+
+internal fun readSecret(file: File): SecretData {
+    val stream = file.inputStream()
+    val serializedData = stream.bufferedReader().use { it.readText() }
+    stream.close()
+    return Json.decodeFromString(serializedData)
 }
