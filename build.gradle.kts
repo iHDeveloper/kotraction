a1f25bb8ddf71b6eab6a8547e9edc357aef36309
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2021-01-02 05:41:03 +0300

    chore: add serialization/fuel packages
    - kotlinx.serialization for de/serializing data classes
    - fuel for using HTTP protocol

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
+val fuelVersion = "2.3.1"
+
 plugins {
     kotlin("jvm") version "1.4.21"
+    kotlin("plugin.serialization") version "1.4.21"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 allprojects {
     apply(plugin = "kotlin")
+    apply(plugin = "kotlinx-serialization")
     apply(plugin = "com.github.johnrengelman.shadow")
 
     group = "me.ihdeveloper"
@@ -17,9 +21,15 @@ allprojects {
     dependencies {
         if (project != rootProject) {
             implementation(kotlin("stdlib"))
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+            implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
+            implementation("com.github.kittinunf.fuel:fuel-kotlinx-serialization:$fuelVersion")
             implementation(rootProject)
         } else {
             compileOnly(kotlin("stdlib"))
+            compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+            compileOnly("com.github.kittinunf.fuel:fuel:$fuelVersion")
+            compileOnly("com.github.kittinunf.fuel:fuel-kotlinx-serialization:$fuelVersion")
         }
     }
 
